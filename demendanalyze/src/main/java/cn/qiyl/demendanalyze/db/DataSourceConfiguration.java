1722cf74b18d3c1afe91dd2fe741989b8cc062d0
QYL <qiyunlong@gmail.com>
2018-10-15 07:55:45 +0800

    1

---
+++ demendanalyze/src/main/java/cn/qiyl/demendanalyze/db/DataSourceConfiguration.java
@@ -0,0 +1,26 @@
+package cn.qiyl.demendanalyze.db;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.sqlite.SQLiteDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * 配置sqlite数据库的DataSource
+ * @author wuweifeng wrote on 2018/3/2.
+ */
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean(destroyMethod = "", name = "EmbeddeddataSource")
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:" + "demendanalyzer.db");
+        dataSourceBuilder.type(SQLiteDataSource.class);
+        return dataSourceBuilder.build();
+    }
+
+}
---
+++ demendanalyze/src/test/java/cn/qiyl/demendanalyze/DemendanalyzeApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.qiyl.demendanalyze;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemendanalyzeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
